538f913a1b879f5837e936f9bfe51bd8608304b4
unnati914 <unnati002btit19@igdtuw.ac.in>
2023-12-06 13:22:45 +0530

    added files

---
+++ server.js
@@ -0,0 +1,9 @@
+const { Configuration, OpenAIApi } = require("openai")
+const express =  require('express')
+const app = express()
+
+app.use(express.static('public'))
+
+app.listen(5000, ()=> {
+    console.log("Server is active")
+})
